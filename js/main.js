8fdc185fa6a3d77fde1e2a7918dbaaa9b1169d05
Murilo Tavares <murilo.s.tavares@hotmail.com>
2023-12-21 11:41:34 -0300

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+$('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email:true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required:true
+            }
+            
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            email: 'Por favor, insira o seu e-mail',
+            telefone: 'Por favor, insira o seu telefone',
+            mensagem: 'Por favor, insira uma mensagem',
+        },
+        submitHandler: function(form) {
+            alert("Mensagem enviada com sucesso!")
+
+            form.reset();
+
+        },
+        invalidHandler: function(evento, validador){
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+
+    })
+    
+})
